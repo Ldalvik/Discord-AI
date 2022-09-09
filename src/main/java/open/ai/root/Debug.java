300bf5a178fdcb90a913d97305b20930195eedf8
Ldalvik <subaruforge@gmail.com>
2022-09-09 13:27:17 -0400

    Official deploy

--- src/main/java/open/ai/root/Debug.java
+++ src/main/java/open/ai/root/Debug.java
@@ -2,9 +2,10 @@ package open.ai.root;
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
 
 public class Debug extends ListenerAdapter {
     @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
     }
 }
