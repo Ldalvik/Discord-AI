41bbbf64cfd2f18d4da1e39ffbb2554cffae70d0
Ldalvik <subaruforge@gmail.com>
2022-09-09 13:32:33 -0400

    Removed duplicate ping command

--- src/main/java/open/ai/root/Main.java
+++ src/main/java/open/ai/root/Main.java
@@ -12,7 +12,6 @@ public class Main {
 
         api.addEventListeners(
                 new ReadyListener(System.currentTimeMillis()),
-                new ChatCommands(),
                 new AiCommands(),
                 new Debug(),
                 new TypeGame(),
--- src/main/java/open/ai/root/PingCommand.java
+++ src/main/java/open/ai/root/PingCommand.java
@@ -1,7 +1,10 @@
 package open.ai.root;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.awt.*;
 
 public class PingCommand extends ListenerAdapter {
 
@@ -9,10 +12,17 @@ public class PingCommand extends ListenerAdapter {
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         if (event.getName().equalsIgnoreCase("ping")) {
             long time = System.currentTimeMillis();
-            event.reply("Pinging...")
+            event.getChannel().sendMessageEmbeds(new EmbedBuilder()
+                            .setColor(Color.cyan)
+                            .setTitle("Ping!")
+                            .addField("Response time", "Pinging...", false).build())
                     .queue(response -> {
                         long responseTime = System.currentTimeMillis() - time;
-                        response.editOriginalFormat("Pong! %d ms", responseTime).queue();
+                        response.editMessageEmbeds(new EmbedBuilder()
+                                        .setColor(Color.cyan)
+                                        .setTitle("Pong!")
+                                        .addField("Response time", responseTime + "ms", false).build())
+                                .queue();
                     });
         }
     }
